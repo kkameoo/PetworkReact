fe30e255cf50140fc67b570056db026583ca97e1
jensen-kor <lukawa1@naver.com>
2025-03-19 16:10:27 +0900

    [준희]받아랏

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,10 @@
 import HomeContents from "../views/main/HomeContents";
 
 function Home() {
-<<<<<<< Updated upstream
   return (
     <>
       <HomeContents />
     </>
   );
-=======
-    return (
-        <>
-            <h2>
-                hello vite react
-            </h2>
-            <HomeContents/>
-        </>
-    )
->>>>>>> Stashed changes
 }
 export default Home;
