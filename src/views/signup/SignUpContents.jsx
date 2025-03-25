f8686264fdf1d84ae38082e00fe1b97675b1c3bf
Justinbeer <96567017+Justinbeer@users.noreply.github.com>
2025-03-25 14:29:25 +0900

    [승환] 회원가입 수정

--- src/views/signup/SignUpContents.jsx
+++ src/views/signup/SignUpContents.jsx
@@ -89,24 +89,122 @@ const ModalButton = styled.button`
   }
 `;
 
+const regions = [
+  [1, "서울시"],
+  [2, "수원시"],
+  [3, "성남시"],
+  [4, "안양시"],
+  [5, "부천시"],
+  [6, "광명시"],
+  [7, "평택시"],
+  [8, "시흥시"],
+  [9, "안산시"],
+  [10, "고양시"],
+  [11, "과천시"],
+  [12, "구리시"],
+  [13, "남양주시"],
+  [14, "오산시"],
+  [15, "화성시"],
+  [16, "인천시"],
+];
+
+const allSis = {
+  1: [
+    [1, "종로구"],
+    [2, "중구"],
+    [3, "용산구"],
+    [4, "성동구"],
+    [5, "광진구"],
+    [6, "동대문구"],
+    [7, "중랑구"],
+    [8, "강북구"],
+    [9, "도봉구"],
+    [10, "노원구"],
+    [11, "은평구"],
+    [12, "서대문구"],
+    [13, "마포구"],
+    [14, "양천구"],
+    [15, "강서구"],
+    [16, "구로구"],
+    [17, "금천구"],
+    [18, "영등포구"],
+    [19, "동작구"],
+    [20, "관악구"],
+    [21, "서초구"],
+    [22, "강남구"],
+    [23, "송파구"],
+    [24, "강동구"],
+  ],
+  2: [
+    [1, "장안구"],
+    [2, "권선구"],
+    [3, "팔달구"],
+    [4, "영통구"],
+  ],
+  3: [
+    [1, "수정구"],
+    [2, "중원구"],
+    [3, "분당구"],
+  ],
+  4: [
+    [1, "만안구"],
+    [2, "동안구"],
+  ],
+  5: [
+    [1, "원미구"],
+    [2, "소사구"],
+    [3, "오정구"],
+  ],
+  6: [[1, "광명구"]],
+  7: [[1, "평택구"]],
+  8: [[1, "시흥구"]],
+  9: [
+    [1, "단원구"],
+    [2, "상록구"],
+  ],
+  10: [
+    [1, "덕양구"],
+    [2, "일산동구"],
+    [3, "일산서구"],
+  ],
+  11: [[1, "과천구"]],
+  12: [[1, "구리구"]],
+  13: [[1, "남양주구"]],
+  14: [[1, "오산구"]],
+  15: [[1, "화성구"]],
+  16: [
+    [1, "중구(인천)"],
+    [2, "동구(인천)"],
+    [3, "미추홀구"],
+    [4, "연수구"],
+    [5, "남동구"],
+    [6, "부평구"],
+    [7, "계양구"],
+    [8, "서구(인천)"],
+    [9, "강화군"],
+    [10, "옹진군"],
+  ],
+};
+
 const SignupContents = () => {
   const [formData, setFormData] = useState({
     name: "",
     nickname: "",
     password: "",
     confirmPassword: "",
-    tel_number: "",
+    telNumber: "",
     email: "",
-    birthdate: "",
-    gender: "",
-    selectedCity: "",
-    selectedDistrict: "",
-    recommend_uid: "",
-    register_date: new Date().toISOString(),
-    is_deleted: false,
-    is_admin: false,
-    upd_date: new Date().toISOString(),
-    verificationCode: "", // ✅ 인증 코드 추가
+    // birthdate: "",
+    // gender: "",
+    preference: 1,
+    localSi: "", // 첫 번째 지역으로 기본값 설정
+    localGu: "", // 첫 번째 군/구로 기본값 설정
+    // recommend_uid: "",
+    // register_date: new Date().toISOString(),
+    // is_deleted: false,
+    // is_admin: false,
+    // upd_date: new Date().toISOString(),
+    // verificationCode: "", // ✅ 인증 코드 추가
   });
   const API_USER_URL = "http://localhost:8087/api/user/register";
   const API_EMAIL_VERIFICATION_URL = `http://localhost:8087/api/email/send`;
@@ -120,67 +218,9 @@ const SignupContents = () => {
   // const [emailVerificationSent, setEmailVerificationSent] = useState(false);
   const [emailInput, setEmailInput] = useState("");
   const navigate = useNavigate();
-  const regionData = {
-    서울시: [
-      "종로구",
-      "중구",
-      "용산구",
-      "성동구",
-      "광진구",
-      "동대문구",
-      "중랑구",
-      "강북구",
-      "도봉구",
-      "노원구",
-      "은평구",
-      "서대문구",
-      "마포구",
-      "양천구",
-      "강서구",
-      "구로구",
-      "금천구",
-      "영등포구",
-      "동작구",
-      "관악구",
-      "서초구",
-      "강남구",
-      "송파구",
-      "강동구",
-    ],
-    수원시: ["장안구", "권선구", "팔달구", "영통구"],
-    성남시: ["수정구", "중원구", "분당구"],
-    안양시: ["만안구", "동안구"],
-    부천시: ["원미구", "소사구", "오정구"],
-    광명시: ["광명구"],
-    평택시: ["평택구", "시흥구", "단원구", "상록구"],
-    시흥시: ["시흥구"],
-    안산시: ["단원구", "상록구"],
-    고양시: ["덕양구", "일산동구", "일산서구"],
-    과천시: ["과천구"],
-    구리시: ["구리구"],
-    남양주시: ["남양주구"],
-    오산시: ["오산구"],
-    화성시: ["화성구"],
-    김포시: ["김포구"],
-    광주시: ["광주구"],
-    하남시: ["하남구"],
-    이천시: ["이천구"],
-    양평군: ["양평군"],
-    동두천시: ["동두천시"],
-    포천시: ["포천시"],
-    인천시: [
-      "중구(인천)",
-      "동구(인천)",
-      "미추홀구",
-      "연수구",
-      "남동구",
-      "부평구",
-      "계양구",
-      "서구(인천)",
-      "강화군",
-      "옹진군",
-    ],
-  };
+
+  // const regionSiText = regions[user.localSi - 1]; // '서울시'
+  // const regionGuText = allSis[regionSiText]?.[user.localGu - 1]; // '강남구'
 
   useEffect(() => {
     fetch(API_USER_URL)
@@ -199,14 +239,35 @@ const SignupContents = () => {
     const { name, value } = e.target;
     setFormData((prev) => ({ ...prev, [name]: value }));
   };
-  const handleRegionChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prev) => ({
-      ...prev,
-      [name]: value,
-      selectedDistrict: "", // 시를 변경하면 군/구는 초기화
-    }));
-  };
+  // 회원가입 시 변환
+  // const handleSignUp = () => {
+  //   const userData = {
+  //     userId,
+  //     password,
+  //     nickname,
+  //     localSi: regions.indexOf(regionSi) + 1,
+  //     localGu: allSis[regionSi]?.indexOf(regionGu) + 1,
+  //   };
+
+  //   console.log("회원가입 데이터:", userData);
+  // };
+  // 회원 정보 조회 시 변환
+  const user = { localSi: 1, localGu: 3 }; // DB에서 가져온 데이터
+
+  // const handleRegionChange = (e) => {
+  //   const selectedSi = e.target.value;
+  //   setFormData((prev) => ({
+  //     ...prev,
+  //     localSi: selectedSi,
+  //     localGu: "", // 시가 바뀌면 구 초기화
+  //   }));
+  // };
+  // const handleDistrictChange = (e) => {
+  //   setFormData((prev) => ({
+  //     ...prev,
+  //     localGu: e.target.value,
+  //   }));
+  // };
 
   const handleSendEmailVerification = async () => {
     if (!emailInput.trim()) {
@@ -258,15 +319,28 @@ const SignupContents = () => {
     }
   };
 
-  // const handleCloseModal = () => {
-  //   setModalVisible(false);
-  // };
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    const phoneRegex = /^\d{10,11}$/;
+    // const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
     console.log(formData);
-    // 회원가입 처리 로직 추가
+
+    const userData = {
+      ...formData,
+      localSi: parseInt(formData.localSi), // 숫자로 변환
+      localGu: formData.localGu ? parseInt(formData.localGu) : null,
+    };
+    // const userData = {
+    //   ...formData,
+    //   localSi: regions.includes(formData.localSi)
+    //     ? regions.indexOf(formData.localSi) + 1
+    //     : null,
+    //   localGu: allSis[formData.localSi]?.includes(formData.localGu)
+    //     ? allSis[formData.localSi].indexOf(formData.localGu) + 1
+    //     : null,
+    // };
+
+    console.log("회원가입 데이터:", userData);
 
     // if (
     //   !formData.name.trim() ||
@@ -281,24 +355,12 @@ const SignupContents = () => {
       alert("이메일 인증을 완료해주세요.");
       return;
     }
-    if (!phoneRegex.test(formData.tel_number)) {
-      alert("전화번호는 숫자만 입력해야 하며, 10~11자리여야 합니다.");
-      return;
-    }
-    if (!passwordMatch) {
-      alert("비밀번호가 일치하지 않습니다.");
-      return;
-    }
-    if (!emailRegex.test(formData.email)) {
-      alert("올바른 이메일 형식을 입력하세요.");
-      return;
-    }
 
     try {
       const response = await fetch(API_USER_URL, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(formData),
+        body: JSON.stringify(userData),
       });
 
       const responseData = await response.text(); // 서버 응답 메시지 받기
@@ -406,75 +468,92 @@ const SignupContents = () => {
         {/* 전화번호 입력 */}
         <Input
           type="text"
-          name="tel_number"
+          name="telNumber"
           placeholder="전화번호"
-          value={formData.tel_number}
+          value={formData.telNumber}
           onChange={handleChange}
         />
         {/* 생년월일 입력 */}
-        <Input
+        {/* <Input
           type="date"
           name="birthdate"
           value={formData.birthdate}
           // onChange={handleChange}
           onChange={handleChange}
-        />
+        /> */}
 
         {/* 시 선택 */}
         <select
-          name="selectedCity"
-          value={formData.selectedCity}
+          name="localSi"
+          value={formData.localSi}
           // onChange={handleRegionChange}
           onChange={(e) => {
             setFormData((prev) => ({
               ...prev,
-              selectedCity: e.target.value,
-              selectedDistrict: "", // ✅ 시 변경 시 군/구 초기화
+              localSi: e.target.value,
+              localGu: "", // ✅ 시 변경 시 군/구 초기화
             }));
           }}
         >
           {/* <option value="">시 선택</option>
-          {regionData.시 &&
-            regionData.시.map((city) => (
-              <option key={city} value={city}>
-                {city}
-              </option>
-            ))} */}
-          <option value="">시 선택</option>
           {Object.keys(regionData).map((city) => (
             <option key={city} value={city}>
               {city}
+            </option> */}
+          <option value="">시 선택</option>
+          {regions.map((city) => (
+            <option key={city} value={city[0]}>
+              {city[1]}
             </option>
           ))}
         </select>
+
         {/* 군구 선택 */}
-        {formData.selectedCity && regionData[formData.selectedCity] && (
+        {formData.localSi && allSis[formData.localSi] && (
           <select
-            name="selectedDistrict"
-            value={formData.selectedDistrict}
-            // onChange={handleRegionChange}
+            name="localGu"
+            value={formData.localGu}
             onChange={(e) => {
               setFormData((prev) => ({
                 ...prev,
-                selectedDistrict: e.target.value,
+                localGu: e.target.value, // 문자열 그대로 저장 (나중에 숫자로 변환)
               }));
             }}
           >
             <option value="">군/구 선택</option>
-            {regionData[formData.selectedCity] &&
-              regionData[formData.selectedCity].map((district) => (
-                <option key={district} value={district}>
-                  {district}
-                </option>
-              ))}
+            {allSis[formData.localSi].map((district) => (
+              <option key={district} value={district[0]}>
+                {district[1]}
+              </option>
+            ))}
           </select>
+          // {/* {formData.localSi && regionData[formData.localSi] && (
+          //   <select
+          //     name="localGu"
+          //     value={formData.localGu}
+          //     // onChange={handleRegionChange}
+          //     onChange={(e) => {
+          //       setFormData((prev) => ({
+          //         ...prev,
+          //         localGu: e.target.value,
+          //       }));
+          //     }}
+          //   >
+          //     <option value="">군/구 선택</option>
+          //     {regionData[formData.localSi] &&
+          //       regionData[formData.localSi].map((district) => (
+          //         <option key={district} value={district}>
+          //           {district}
+          //         </option>
+          //       ))}
+          //   </select> */}
         )}
         {/* 성별 선택 */}
-        <select name="gender" value={formData.gender} onChange={handleChange}>
+        {/* <select name="gender" value={formData.gender} onChange={handleChange}>
           <option value="">성별 선택</option>
           <option value="male">남성</option>
           <option value="female">여성</option>
-        </select>
+        </select> */}
         {/* 회원가입 버튼 */}
         <Button type="submit">가입하기</Button>
       </Form>
