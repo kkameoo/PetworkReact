84f457409106af4ea8a645754a975ba52b9ea14c
cheowha1 <cheowha1@naver.com>
2025-03-26 17:21:39 +0900

    DESIGN : 알림기능 추가 밑 연동

---
+++ src/hooks/socket.js
@@ -0,0 +1,20 @@
+import { Client } from "@stomp/stompjs";
+import SockJS from "sockjs-client";
+const socket = new SockJS("http://localhost:8087/ws");
+const stompClient = new Client({
+  webSocketFactory: () => socket,
+  reconnectDelay: 5000,
+});
+let onNotificationCallback = null;
+export const connectSocket = (userId, onNotification) => {
+  onNotificationCallback = onNotification;
+  stompClient.onConnect = () => {
+    stompClient.subscribe(`/user/${userId}/notification`, (message) => {
+      if (onNotificationCallback) {
+        onNotificationCallback(JSON.parse(message.body));
+      }
+    });
+  };
+  stompClient.activate();
+};
+export default stompClient;
