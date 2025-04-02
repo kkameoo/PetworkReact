d2386cd2ebd6bf66fa69d995cb11674a175e7863
cheowha1 <cheowha1@naver.com>
2025-04-02 16:49:31 +0900

    DESIGN 수정

--- src/views/main/MainPage.jsx
+++ src/views/main/MainPage.jsx
@@ -312,7 +312,7 @@ function MainPage() {
     <PageLayout>
       <ContentArea>
         <SectionWrapper>
-          <SectionTitle>🐾산책 게시판 인기글🐾</SectionTitle>
+          <SectionTitle>산책 게시판 인기글 🐾</SectionTitle>
           <PostsWrapper>
             {homePosts.map((post) =>
               renderPostCard(post, imageMap[post.boardId], "walk")
@@ -321,7 +321,7 @@ function MainPage() {
         </SectionWrapper>
 
         <SectionWrapper>
-          <SectionTitle>🎁나눔 게시판 인기글🎁</SectionTitle>
+          <SectionTitle>나눔 게시판 인기글 🎁</SectionTitle>
           <PostsWrapper>
             {tradePosts.map((post) =>
               renderPostCard(post, imageMap[post.boardId], "trade")
@@ -330,7 +330,7 @@ function MainPage() {
         </SectionWrapper>
 
         <SectionWrapper>
-          <SectionTitle>🤝알바 게시판 인기글🤝</SectionTitle>
+          <SectionTitle>알바 게시판 인기글 🤝</SectionTitle>
           <PostsWrapper>
             {jobPosts.map((post) =>
               renderPostCard(post, imageMap[post.boardId], "hire")
@@ -338,7 +338,7 @@ function MainPage() {
           </PostsWrapper>
         </SectionWrapper>
         <SectionWrapper>
-          <SectionTitle>📸펫스타그램 인기글📸</SectionTitle>
+          <SectionTitle>펫스타그램 인기글 📸</SectionTitle>
           <PostsWrapper>
             {PopularPosts.map((post) =>
               renderPostCard(post, imageMap[post.boardId], "petstar")
